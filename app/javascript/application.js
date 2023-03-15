634aba1a9605e8d143bc914e3a1e2b8696580e69
AlbanianSilka <andrey2096@gmail.com>
2023-03-15 20:27:18 +0200

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Entrypoint for the esbuild command defined in package.json scripts
+
+import "bootstrap/dist/js/bootstrap.bundle"
+import "bootstrap/dist/css/bootstrap.css"
+// to add event listeners it is enough to add file to "javascript folder" and import it here
+// !! do not forget to enter command "yarn build --watch" every time before running server
